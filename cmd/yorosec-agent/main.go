e17245f889c013465f19d1f45f5ba7aba662c244
johnnychiu <johnnyychiu@gmail.com>
2025-09-08 11:28:19 +0100

    chore: init project skeleton

---
+++ cmd/yorosec-agent/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("yorosec-agent v0.0.1")
+	fmt.Println("Usage: yoro [scan|report|version]")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/YorozuyaSec/yorosec-agent
+
+go 1.19
